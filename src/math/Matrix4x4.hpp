e165711fe39e09ee3f7171d750d1ef88059e1c2e
Pimerroc <solntsevmaxm1@yandex.ru>
2025-11-15 19:04:51 +0300

    Pull changes

--- src/math/Matrix4x4.hpp
+++ src/math/Matrix4x4.hpp
@@ -1,20 +1,24 @@
 #pragma once
-
-#include "vector4.hpp"
+#include <array>
+#include "Vector4.hpp"
 
 class Matrix4x4 {
 public:
-    float m[4][4];
-
+    void set(const std::array<float,16>& values);
     Matrix4x4();
-
+    
     static Matrix4x4 identity();
     Matrix4x4 transposed() const;
+
+    float get(int row, int col) const;
+    void set_element(int row, int col, float value);
 
     Matrix4x4 operator+(const Matrix4x4& other) const;
     Matrix4x4 operator-(const Matrix4x4& other) const;
     Matrix4x4 operator*(const Matrix4x4& other) const;
 
     Vector4 operator*(const Vector4& vec) const;
+private:
+    std::array<float,16> m_;
 };
 
