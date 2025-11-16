e14255597cbca6f408c7c8054c7ce3f95e67a044
robustov <yaroslavrobustov@yandex.ru>
2025-11-16 19:45:28 +0300

    blyat sorry this is a normal Mesh class this time fr fr trust me pls

--- src/core/Mesh.hpp
+++ src/core/Mesh.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <cstddef>
 #include <vector>
 
 #include "math/UVCoord.hpp"
@@ -21,7 +22,6 @@ public:
 
   Mesh() = default;
 
-  // <-- declaration only (note the trailing semicolon)
   Mesh(std::vector<math::Vector3> vertices,
        std::vector<math::UVCoord> texture_vertices,
        std::vector<math::Vector3> normals,
@@ -29,7 +29,17 @@ public:
 
   ~Mesh();
 
-  // ...
+  Mesh(const Mesh &) = default;
+  Mesh(Mesh &&) noexcept = default;
+  Mesh &operator=(const Mesh &) = default;
+  Mesh &operator=(Mesh &&) noexcept = default;
+
+  std::size_t vertex_count() const noexcept { return vertices.size(); }
+  std::size_t texcoord_count() const noexcept {
+    return texture_vertices.size();
+  }
+  std::size_t normal_count() const noexcept { return normals.size(); }
+  std::size_t face_count() const noexcept { return faces.size(); }
 };
 
 } // namespace di_renderer::core
