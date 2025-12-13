692af65ea6feff6f13c3c16dbdf3dd6dc093c291
Pimerroc <solntsevmax1@yandex.ru>
2025-12-13 20:18:01 +0300

    Change tests

--- tests/test_rendering.cpp
+++ tests/test_rendering.cpp
@@ -1,4 +1,3 @@
-#include "math/MatrixTransforms.hpp"
 #include "render/Camera.hpp"
 
 #include <cmath>
@@ -7,7 +6,7 @@ using namespace di_renderer::render;
 using namespace di_renderer::math;
 
 TEST(CameraTests, ViewMatrix_Identity) {
-    Camera cam({0, 0, 0}, {0, 0, 1}, 1.0F, 1.0F, 0.1F, 100.0F);
+    Camera cam({0, 0, 0}, {0, 0, -1}, 1.0F, 1.0F, 0.1F, 100.0F);
     // clang-format off
     Matrix4x4 expected({
         1.0F, 0.0F, 0.0F, 0.0F,
@@ -21,13 +20,13 @@ TEST(CameraTests, ViewMatrix_Identity) {
 }
 
 TEST(CameraTests, ViewMatrix_TranslationZ) {
-    Camera cam({0, 0, -10}, {0, 0, 0}, 1.0F, 1.0F, 0.1F, 100.0F);
+    Camera cam({0, 0, 10}, {0, 0, 0}, 1.0F, 1.0F, 0.1F, 100.0F);
     // clang-format off
     Matrix4x4 expected({
         1.0F, 0.0F, 0.0F, 0.0F,
         0.0F, 1.0F, 0.0F, 0.0F,
-        0.0F, 0.0F, 1.0F, 10.0F,
-        0.0F, 0.0F, 0.0F, 1.0F
+        0.0F, 0.0F, 1.0F, 0.0F,
+        0.0F, 0.0F, -10.0F, 1.0F
     });
     // clang-format on
 
@@ -35,14 +34,14 @@ TEST(CameraTests, ViewMatrix_TranslationZ) {
 }
 
 TEST(CameraTests, ViewMatrix_TranslationX_Strafing) {
-    Camera cam({0, 0, 0}, {0, 0, 1}, 1.0F, 1.0F, 0.1F, 100.0F);
+    Camera cam({0, 0, 0}, {0, 0, -1}, 1.0F, 1.0F, 0.1F, 100.0F);
     cam.move({5.0F, 0.0F, 0.0F});
     // clang-format off
     Matrix4x4 expected({
-        1.0F, 0.0F, 0.0F, -5.0F,
+        1.0F, 0.0F, 0.0F, 0.0F,
         0.0F, 1.0F, 0.0F, 0.0F,
         0.0F, 0.0F, 1.0F, 0.0F,
-        0.0F, 0.0F, 0.0F, 1.0F
+        -5.0F, 0.0F, 0.0F, 1.0F
     });
     // clang-format on
 
@@ -53,12 +52,12 @@ TEST(CameraTests, ViewMatrix_Rotation_90Degrees) {
     // Z (Forward) = (1, 0, 0)
     // X (Right)   = Up x Z = (0, 0, -1)
     // Y (New Up)  = Z x X  = (0, 1, 0)
-    Camera cam({0, 0, 0}, {1, 0, 0}, 1.0F, 1.0F, 0.1F, 100.0F);
+    Camera cam({0, 0, 0}, {-1, 0, 0}, 1.0F, 1.0F, 0.1F, 100.0F);
     // clang-format off
     Matrix4x4 expected({
-        0.0F, 0.0F, -1.0F, 0.0F,
+        0.0F, 0.0F, 1.0F, 0.0F,
         0.0F, 1.0F,  0.0F, 0.0F,
-        1.0F, 0.0F,  0.0F, 0.0F,
+        -1.0F, 0.0F,  0.0F, 0.0F,
         0.0F, 0.0F,  0.0F, 1.0F
     });
     // clang-format on
@@ -77,15 +76,15 @@ TEST(CameraTests, ProjectionMatrix_Calculated) {
     // tan(45) = 1.
     // [0,0] = 1/tan = 1
     // [1,1] = 1/(2*1) = 0.5
-    // [2,2] = (3+1)/(3-1) = 2
-    // [2,3] = 2*3*1/2 = 3
-    // [3,2] = 1
+    // [2,2] = -(3+1)/(3-1) = -2
+    // [2,3] = -(2*3*1/2) = -3
+    // [3,1] = -1
     // clang-format off
     Matrix4x4 expected({
         1.0F, 0.0F, 0.0F, 0.0F,
         0.0F, 0.5F, 0.0F, 0.0F,
-        0.0F, 0.0F, 2.0F, 3.0F,
-        0.0F, 0.0F, 1.0F, 0.0F
+        0.0F, 0.0F, -2.0F, -1.0F,
+        0.0F, 0.0F, -3.0F, 0.0F
     });
     // clang-format on
 
@@ -93,15 +92,15 @@ TEST(CameraTests, ProjectionMatrix_Calculated) {
 }
 
 TEST(CameraTests, Setters_UpdateMatrices) {
-    Camera cam({0, 0, 0}, {0, 0, 1}, 1.0F, 1.0F, 0.1F, 100.0F);
+    Camera cam({0, 0, 0}, {0, 0, -1}, 1.0F, 1.0F, 0.1F, 100.0F);
 
     cam.move({0, 10, 0});
     // clang-format off
     Matrix4x4 expected({
         1.0F, 0.0F, 0.0F, 0.0F,
-        0.0F, 1.0F, 0.0F, -10.0F,
+        0.0F, 1.0F, 0.0F, 0.0F,
         0.0F, 0.0F, 1.0F, 0.0F,
-        0.0F, 0.0F, 0.0F, 1.0F
+        0.0F, -10.0F, 0.0F, 1.0F
     });
     // clang-format on
 
