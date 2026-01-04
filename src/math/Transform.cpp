755b37fdf16ac7b95200edfead5cbe672ee23c10
yel-gar <231206328+yel-gar@users.noreply.github.com>
2026-01-04 21:32:42 +0300

    Remove unnecessary includes in Transform

--- src/math/Transform.cpp
+++ src/math/Transform.cpp
@@ -2,9 +2,6 @@
 
 #include "math/MatrixTransforms.hpp"
 
-#include <iostream>
-#include <ostream>
-
 namespace di_renderer::math {
     // rotation в радианах!
     Transform::Transform() : m_translation(0.0f, 0.0f, 0.0f), m_rotation(0.0f, 0.0f, 0.0f), m_scale(1.0f, 1.0f, 1.0f) {}
